5c87ca1072845e5f07b29e44e4a8116b21f46d8c
Pavlo Ocheretianyi <pavel_ocheretyany_01@mail.ua>
2020-08-03 13:06:11 +0300

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace Clock
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromMilliseconds(1);
+            timer.Tick += Timer_Tick;
+            timer.Start();
+        }
+        void Timer_Tick(object sender, EventArgs e)
+        {
+            lblTime.Content = DateTime.Now.ToString("HH:mm:ss.fff");
+        }
+    }
+}
